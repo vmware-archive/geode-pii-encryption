5254d6ecb88ccc713d94ee8e518d73c30611a789
nsarvi <nsarvi@pivotal.io>
2018-09-13 10:46:08 -0500

    added tests

--- crypto/src/test/java/io/pivotal/pde/geode/CryptoTest.java
+++ crypto/src/test/java/io/pivotal/pde/geode/CryptoTest.java
@@ -12,7 +12,7 @@ public class CryptoTest {
 
 
     @Test
-    public void testEncryptName() {
+    public void testCryptoName() {
 
         ICryptoService cryptoService = CryptoUtil.getCipher(CipherType.AES, "0123456789012345".getBytes(), "01234567890123456789012345678901".getBytes());
         String value = "Gideon and Niranjan";
@@ -23,7 +23,7 @@ public class CryptoTest {
     }
 
     @Test
-    public void testEncryptAddress() {
+    public void testCryptoAddress() {
         ICryptoService cryptoService = CryptoUtil.getCipher(CipherType.AES, "0123456789012345".getBytes(), "01234567890123456789012345678901".getBytes());
 
         String value = "222, Main Street, Apt 202";
@@ -34,37 +34,16 @@ public class CryptoTest {
     }
 
     @Test
-    public void testEncryptDecrypt3() {
+    public void testCryptoCreditCard() {
 
         ICryptoService cryptoService = CryptoUtil.getCipher(CipherType.AES, "0123456789012345".getBytes(), "01234567890123456789012345678901".getBytes());
 
-        String value = "gideon.low@garmin.com";
+        String value = "4567 1234 5678 1234";
 
         String encryptedValue = cryptoService.encrypt(value);
         String decryptedValue = cryptoService.decrypt(encryptedValue);
         assertThat(value, equalTo(decryptedValue));
     }
 
-    @Test
-    public void testEncryptDecrypt4() {
 
-        ICryptoService cryptoService = CryptoUtil.getCipher(CipherType.AES, "0123456789012345".getBytes(), "01234567890123456789012345678901".getBytes());
-
-        String value = "Nsarvi1.Sarvi23$.3342@garmin.com";
-
-        String encryptedValue = cryptoService.encrypt(value);
-        String decryptedValue = cryptoService.decrypt(encryptedValue);
-        assertThat(value, equalTo(decryptedValue));
-    }
-
-    @Test
-    public void testEncryptDecrypt5() {
-        ICryptoService cryptoService = CryptoUtil.getCipher(CipherType.AES, "0123456789012345".getBytes(), "01234567890123456789012345678901".getBytes());
-
-        String value = "Gideon12.low2!*3ywdd@abc.uk";
-
-        String encryptedValue = cryptoService.encrypt(value);
-        String decryptedValue = cryptoService.decrypt(encryptedValue);
-        assertThat(value, equalTo(decryptedValue));
-    }
 }
